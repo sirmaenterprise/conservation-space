cf7e802b4362ac5ceed61576390596ece62e5c19
bbanchev <borislav.banchev@sirma.bg>
2014-12-05 21:02:27 +0200

    updated build scripts

--- alfresco-build.sh
+++ alfresco-build.sh
@@ -2,6 +2,17 @@
 work_dir=$(readlink -f $(dirname "$0"))
 home_dir=$(getent passwd $SUDO_USER | cut -d: -f6)
 
-
+cd ${work_dir}/alfresco/alfresco-parent
+mvn install -N
+cd ${work_dir}/alfresco/activiti-engine
+mvn install
+cd ${work_dir}/alfresco/java-opensaml2-main/
+mvn install -DskipTests=true
+cd ${work_dir}/alfresco/alfresco-emf-integration/
+mvn install -N
+cd ${work_dir}/alfresco/alfresco-emf-integration/alfresco-integration-api
+mvn install
 cd ${work_dir}/alfresco/alfresco-emf-integration/alfresco-integration-impl
+mvn install
+cd ${work_dir}/alfresco/alfresco-emf-integration/
 mvn install
